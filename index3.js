302f6582363051eb2083fd7d1a1d9f248c76dc53
harlinsonvasquez <harvack25@gmail.com>
2023-12-13 15:56:29 -0500

    Add files via upload

--- index3.js
+++ index3.js
@@ -1,27 +1,21 @@
-function enviarRespuesta() {
-    let nombre = document.getElementById("nombre").value;
-    let apellidos = document.getElementById("apellidos").value;
-    let edad = document.getElementById("edad").value;
+function registrar(){
+    let strNombre=document.getElementById("nombre");
+    let strApellidos=document.getElementById("apellidos")
+    let edad=document.getElementById("edad")
+    
+  }
 
-    // Asignar los valores a los campos correspondientes
-    document.getElementById("nombreInput").value = nombre;
-    document.getElementById("apellidosInput").value = apellidos;
-    document.getElementById("edadInput").value = edad;
-
-    // Obtener valores de linaje y cualidades
-    let linaje = document.getElementById("opcioneslinaje").value;
-    let cualidades = document.getElementById("opcionesCualidades").value;
-
-    // Actualizar valores en los campos linajeInput y cualidadesInput
-    respuestalinaje(linaje);
-    listacualidades(cualidades);
+function respuesta() {
+    let respuesta = document.getElementById('opcioneslinaje');
+    let linaje = respuesta.value;
+   
+    respuestalinaje(linaje)
 }
 
-
 function respuestalinaje(linaje) {
-    let respuestaInput = document.getElementById('linajeInput');
+    let respuestaInput = document.getElementById('respuestaInput');
     
-    switch (linaje) {
+    switch(linaje){
         case "1":
             respuestaInput.value = "mestizo";
             break;
@@ -32,36 +26,42 @@ function respuestalinaje(linaje) {
             respuestaInput.value = "sangre pura";
             break;
         default:
-            console.log("Opción no válida");
+            console.log("opción no válida");
     }
 }
 
-function listacualidades(cualidades) {
-    let respuestaCualidades1 = document.getElementById('cualidadesInput');
+function respuestaCualidades() {
+    let respuestaCualidades = document.getElementById('opcionesCualidades');
+    let cualidades = respuestaCualidades.value;
+  
+    listacualidades(cualidades);
+}
 
-    switch (cualidades) {
+function listacualidades(cualidades){
+    let respuestaCualidades1 = document.getElementById('respuestaCualidades1');
+
+    switch(cualidades){
         case "1":
-            respuestaCualidades1.value = "Valor,fuerza,audacia";
+            respuestaCualidades1.value="Valor,fuerza,audacia";
             break;
         case "2":
-            respuestaCualidades1.value = "Justicia,Lealtad,Paciencia";
+            respuestaCualidades1.value="justicia,Lealtad,Paciencia";
             break;
         case "3":
-            respuestaCualidades1.value = "Creatividad,Erudición,Inteligencia";
+            respuestaCualidades1.value="Creatividad,Erudición,Inteligencia"
             break;
         case "4":
-            respuestaCualidades1.value = "Ambición,Determinación,Astucia";
+            respuestaCualidades1.value="Ambición,Determinación,Astucia"
             break;
         default:
-            console.log("Opción no válida");
+            console.log("opción no válida")
     }
 }
-
 
 function obtenercasa(){
     let casa = document.getElementById('casa');
-    let cualidadEstudiante = document.getElementById('cualidadesInput');
-    let linajeEstudiante = document.getElementById('linajeInput');
+    let cualidadEstudiante = document.getElementById('respuestaCualidades1');
+    let linajeEstudiante = document.getElementById('respuestaInput');
 
     if (
         (cualidadEstudiante.value === "Valor,fuerza,audacia" && 
@@ -71,7 +71,7 @@ function obtenercasa(){
     ) {
         casa.value = casas.casa1;
     } else if (
-        cualidadEstudiante.value === "Justicia,Lealtad,Paciencia" && 
+        cualidadEstudiante.value === "justicia,Lealtad,Paciencia" && 
         (linajeEstudiante.value === "mestizo" || linajeEstudiante.value === "muggle")
     ) {
         casa.value = casas.casa2;
@@ -111,3 +111,70 @@ const casas ={
     casa3:'ravenclaw',
     casa4:'slytherin'
 }
+
+
+
+
+
+
+const claseTransformaciones = {
+    materia: 'transformaciones',
+    profe: 'kevin slughorn',
+    horario: '9:30 am',
+    boggarts: 'abejas',
+    antiboggarts:'cachorros de perritos',
+    
+    realizarTransformacionRiddikulus: function() {
+      if (this.boggarts !== undefined) {
+        this.boggarts = this.antiboggarts;
+        console.log(this.boggarts);
+      }else{
+        console.log("no hay boggart con propiedades")
+      }
+    },
+
+    enfrentarBoggart:function(boggarts){
+        console.log(`se enfrentan el boggarts ${this.boggarts} contra el antiboggarts ${this.antiboggarts}`)
+
+    }
+  };
+  
+  // Llamando al método del objeto claseTransformaciones
+  claseTransformaciones.enfrentarBoggart();
+  claseTransformaciones.realizarTransformacionRiddikulus();
+
+  const boggartEjemplo ={
+
+    boggarts:'leon'
+   
+
+  }/*
+  boggartEjemplo.boggarts=claseTransformaciones.realizarTransformacionRiddikulus;
+
+  boggartEjemplo.boggarts();
+  console.log(boggartEjemplo.boggarts)*/
+  /*claseTransformaciones.enfrentarBoggart(boggartEjemplo.boggarts)
+
+  let efensaContraLasArtesOscuras ={
+    animal1:'ciervo',
+    animal2:'lobo',
+    animal3:'raton',
+    animal4:'perro'
+  }
+  let indiceAleatorio=Math.floor(Math.random()*4)+1;
+  let propiedadAleatoria = "animal" + indiceAleatorio;
+
+  estudiante.animal=efensaContraLasArtesOscuras[propiedadAleatoria];
+  console.log(estudiante.animal)
+  //estudiante.animal=undefined;
+
+  function dementor(){
+    if(estudiante.animal!==undefined){
+        console.log(`su animal patronus ${estudiante.animal} vencio el dementor`)
+    }else{
+        console.log("el estudiante no tenia animal patronus fue absorbido y llevado a enfermeria")
+    }
+
+  }
+  dementor();
+  console.table(estudiante)*/
